13bc55753319e27dd096b1f607c55c1ad67e4cf4
Alexander Mueller <XelaRellum@web.de>
2021-02-18 22:26:19 +0100

    add testcases for Kettenantrieb.ino

--- Kettenantrieb_test.cpp
+++ Kettenantrieb_test.cpp
@@ -102,6 +102,8 @@ extern "C" {
 #define map my_map
 
 // include the actual source code as header
+#include "Kettenantrieb.ino"
+
 #undef map
 }
 
@@ -130,4 +132,114 @@ TEST_CASE("test my_map (map implementation)")
         REQUIRE(my_map(10, 20, 10, 200, 100) == 100);
         REQUIRE(my_map(20, 20, 10, 200, 100) == 200);
     }
+}
+
+struct Fixture
+{
+    Fixture()
+    {
+        m_ArduinoStub = ArduinoStub();
+    }
+};
+
+
+TEST_CASE_METHOD(Fixture, "single run to check for errors")
+{
+    setup();
+    loop();
+}
+
+/**
+ * just some aliases so we can use symbolic names
+ * and not comments
+ */
+enum
+{
+    A_XAXIS = A0,
+    A_YAXIS = A1,
+    Y_NEUTRAL = 512,
+    Y_FULL_BACKWARDS = 0,
+    Y_FULL_FORWARD = 1023,
+    X_NEUTRAL = 512,
+    X_FULL_LEFT = 0,
+    X_FULL_RIGHT = 1023,
+    MOTOR_A_1 = in1,
+    MOTOR_A_2 = in2,
+    MOTOR_B_1 = in3,
+    MOTOR_B_2 = in4,
+};
+
+
+TEST_CASE_METHOD(Fixture, "both joysticks in the middle, both motors should be off")
+{
+    // ARRANGE
+    setup();
+    m_ArduinoStub.ad[A_XAXIS] = X_NEUTRAL;
+    m_ArduinoStub.ad[A_YAXIS] = Y_NEUTRAL;
+
+    // EXECUTE
+    loop();
+
+    // ASSERT
+    REQUIRE(m_ArduinoStub.pwm[enA] == 0);
+    REQUIRE(m_ArduinoStub.pwm[enB] == 0);
+}
+
+
+TEST_CASE_METHOD(Fixture, "full forward, both motors forward")
+{
+    // ARRANGE
+    setup();
+    m_ArduinoStub.ad[A_XAXIS] = X_NEUTRAL;
+    m_ArduinoStub.ad[A_YAXIS] = Y_FULL_FORWARD;
+
+    // EXECUTE
+    loop();
+
+    // ASSERT
+    // both motors full power?
+    REQUIRE(m_ArduinoStub.pwm[enA] == 255);
+    REQUIRE(m_ArduinoStub.pwm[enB] == 255);
+
+    // both motors forward?
+    REQUIRE(m_ArduinoStub.outputs[MOTOR_A_1] == LOW);
+    REQUIRE(m_ArduinoStub.outputs[MOTOR_B_1] == LOW);
+}
+
+
+TEST_CASE_METHOD(Fixture, "full forward and left, left motor off, right motor forward")
+{
+    // ARRANGE
+    setup();
+    m_ArduinoStub.ad[A_XAXIS] = X_FULL_LEFT;
+    m_ArduinoStub.ad[A_YAXIS] = Y_FULL_FORWARD;
+
+    // EXECUTE
+    loop();
+
+    // ASSERT
+    REQUIRE(m_ArduinoStub.pwm[enA] == 0);
+    REQUIRE(m_ArduinoStub.pwm[enB] == 255);
+
+    // REQUIRE(m_ArduinoStub.outputs[MOTOR_A_1] == HIGH); // backwards?
+    REQUIRE(m_ArduinoStub.outputs[MOTOR_B_1] == LOW); // forward?
+}
+
+
+TEST_CASE_METHOD(Fixture, "full forward and right, left motor forward, right motor off")
+{
+    // ARRANGE
+    setup();
+    m_ArduinoStub.ad[A_XAXIS] = X_FULL_RIGHT;
+    m_ArduinoStub.ad[A_YAXIS] = Y_FULL_FORWARD;
+
+    // EXECUTE
+    loop();
+
+    // ASSERT
+    REQUIRE(m_ArduinoStub.pwm[enA] == 255);
+    REQUIRE(m_ArduinoStub.pwm[enB] == 0);
+
+    REQUIRE(m_ArduinoStub.outputs[MOTOR_A_1] == LOW); // forward?
+    // REQUIRE(m_ArduinoStub.outputs[MOTOR_B_1] == LOW); // forward?
 }
